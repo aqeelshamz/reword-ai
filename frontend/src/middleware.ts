d8906821ec2ce4f34b8e96b53ff9b6e8f7b07e6d
Aqeel <aqeelten@gmail.com>
2024-04-25 15:31:43 +0530

    middleware error fix

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,15 +3,21 @@ import { serverURL } from "./utils/utils";
 
 export default async function middleware(req: NextRequest) {
     const token = req.cookies.get('token')?.value;
-    var response = await fetch(`${serverURL}/users`, { method: "GET", credentials: "include", headers: { Authorization: `Bearer ${token}` }, });
-    //print the response
-    const user = await response.json();
+    if (token) {
+        var response = await fetch(`${serverURL}/users`, { method: "GET", credentials: "include", headers: { Authorization: `Bearer ${token}` }, });
 
-    if (req.nextUrl.pathname === '/admin') {
-        return NextResponse.redirect(new URL('/admin/dashboard', req.url))
+        //print the response
+        const user = await response.json();
+
+        if (req.nextUrl.pathname === '/admin') {
+            return NextResponse.redirect(new URL('/admin/dashboard', req.url))
+        }
+
+        if (req.nextUrl.pathname.startsWith("/admin") && user.type !== 'admin') {
+            return NextResponse.redirect(new URL('/home', req.url))
+        }
     }
-
-    if (req.nextUrl.pathname.startsWith("/admin") && user.type !== 'admin') {
-        return NextResponse.redirect(new URL('/home', req.url))
+    else{
+        return NextResponse.redirect(new URL('/login', req.url))
     }
 }
