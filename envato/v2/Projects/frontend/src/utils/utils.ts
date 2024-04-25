4fc28a969e42d66602a7f5eeb047d7a311876f53
Aqeel <aqeelten@gmail.com>
2024-04-25 11:41:16 +0530

    documentation, screenshots

--- envato/v2/Projects/frontend/src/utils/utils.ts
+++ envato/v2/Projects/frontend/src/utils/utils.ts
@@ -1,6 +1,6 @@
 const serverURL = "http://localhost:8080"
 const currencySymbol = "₹";
 const appName = "RewordAI";
-const stripeKey = "pk_test_SCTPV4fZ9kMlryTRZ6XXzgDac90D00AQLTb51NaV7yFrkiQqJuN9j1IlzllFXnYSyCU1BVYpyXxBzycVuM5jTVFKB5FauiR3c5";
+const stripeKey = "YOUR_STRIPE_KEY";
 
 export { serverURL, currencySymbol, appName, stripeKey };
