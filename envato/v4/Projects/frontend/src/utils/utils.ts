d66132e413bf12cbd6fb759ef58c7381ebff595c
Aqeel <aqeelten@gmail.com>
2024-10-03 11:03:34 +0530

    v4 - GPT 4o mini, docs update, detailed docs for hosting

---
+++ envato/v4/Projects/frontend/src/utils/utils.ts
@@ -0,0 +1,6 @@
+const serverURL = "http://localhost:8080"
+const currencySymbol = "₹";
+const appName = "RewordAI";
+const stripeKey = "YOUR_STRIPE_KEY";
+
+export { serverURL, currencySymbol, appName, stripeKey };
